b0ead7652bcc1676a534563e1a41e1f2d425a9fa
Rajat Bhardwaj <43852144+RajatBhardwaj007@users.noreply.github.com>
2020-05-12 21:45:06 +0530

    Update code_to_convert_xml_to_excel.py

--- code_to_convert_xml_to_excel.py
+++ code_to_convert_xml_to_excel.py
@@ -2,17 +2,21 @@
 import xml.etree.ElementTree as ET #this module formats data in tree struture(heirarchial form)
 import xlwt    
 from xlwt import Workbook #To generate sheets and store data in them 
+wb = Workbook() 
+sheet1 = wb.add_sheet('Sheet 1')
+
 mytree = ET.parse('data.xml.xml')
 myroot= mytree.getroot()
 #print(myroot.tag) [for head node of data] 
-wb = Workbook() 
-sheet1 = wb.add_sheet('Sheet 1')    
+  
 z=1
 ct=0
 for x in myroot[1].findall('Stroke'):
     ct=ct+1
+
 #print no. of strokes    
- print(ct)
+print(ct)
+ 
 sheet1.write(0, 0, "X_value")
 sheet1.write(0, 1, "Y_value")
 sheet1.write(0, 2, "Time")
@@ -23,7 +27,10 @@ for x in myroot[1]:
         sheet1.write(z,1,y.get('y'))
         sheet1.write(z,2,y.get('time'))
         z=z+1
-    
+    sheet1.write(z,0,'\n')
+    sheet1.write(z,1,'\n')
+    sheet1.write(z,2,'\n')
+    z=z+1
 
 #saving data from sheet to xls file
 wb.save('datafile.xls')
