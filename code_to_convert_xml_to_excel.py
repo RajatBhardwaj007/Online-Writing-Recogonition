747339e1d1062d028b791d1f543f36b188b50f23
Rajat Bhardwaj <43852144+RajatBhardwaj007@users.noreply.github.com>
2020-05-12 21:34:42 +0530

    Update code_to_convert_xml_to_excel.py

--- code_to_convert_xml_to_excel.py
+++ code_to_convert_xml_to_excel.py
@@ -8,7 +8,11 @@ myroot= mytree.getroot()
 wb = Workbook() 
 sheet1 = wb.add_sheet('Sheet 1')    
 z=1
-
+ct=0
+for x in myroot[1].findall('Stroke'):
+    ct=ct+1
+#print no. of strokes    
+ print(ct)
 sheet1.write(0, 0, "X_value")
 sheet1.write(0, 1, "Y_value")
 sheet1.write(0, 2, "Time")
